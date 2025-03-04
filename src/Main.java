d571cedc9efba56c5ba1565f91b2e30673fdf167
Daria-Voloshanovska <voloshanovskadaria@gmail.com>
2025-03-04 21:02:55 +0100

    add 2 exercise

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,34 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-List<Programmer> programmers  = new ArrayList<>();
-        programmers .add(new Programmer("John","London",List.of(
-        new Task(1,"Fix bug","In progress",3),
-        new Task(2,"Write tests","To Do", 0))));
-        programmers .add(new Programmer("John","Rio",List.of(
-        new Task(3,"Review code","Done",5),
-        new Task(4,"Deploy application","In Progress",2)
-)));
+        List<Programmer> programmers = new ArrayList<>();
+        programmers.add(new Programmer("John", "Berlin", List.of(
+                new Task(1, "Fix bug", "In progress", 10),
+                new Task(2, "Write tests", "Done", 3))));
+        programmers.add(new Programmer("John", "Berlin", List.of(
+                        new Task(3, "Refactor code", "Done", 7),
+                        new Task(4, "Deploy application", "In Progress", 2))));
+                programmers.add(new Programmer("Charlie", "Munich", List.of(
+                        new Task(5, "Code review", "In Progress", 6))));
 
-List<Task> allTasks = getAllTasks(programmers);
 
-allTasks.forEach(task -> System.out.println(
-        "Task " + task.getNumber() + " : " + task.getDescription() + " (" + task.getStatus() + ", " + task.getDaysInProcessing() + " days)"
-));
+        List<Task> filteredTasks = programmers.stream()
+                .filter(p -> "Berlin".equals(p.getCity()))
+                .flatMap(p -> p.getTasks().stream())
+                .filter(task -> !"Done".equalsIgnoreCase(task.getStatus()) && task.getDaysInProcessing() > 5)
+                .toList();
+        filteredTasks.forEach(s -> System.out.println(s));
+
+        List<Task> allTasks = getAllTasks(programmers);
+
+        allTasks.forEach(task -> System.out.println(
+                "Task " + task.getNumber() + " : " + task.getDescription() + " (" + task.getStatus() + ", " + task.getDaysInProcessing() + " days)"
+        ));
     }
-public static List<Task> getAllTasks(List<Programmer> p){
+
+    public static List<Task> getAllTasks(List<Programmer> p) {
         return p.stream()
                 .flatMap(list -> list.getTasks().stream())
                 .toList();
-}
+    }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -13,12 +14,15 @@ class MainTest {
     @BeforeEach
     void setUp() {
         programmers  = new ArrayList<>();
-        programmers .add(new Programmer("John","London",List.of(
-                new Task(1,"Fix bug","In progress",3),
+        programmers .add(new Programmer("John","Berlin",List.of(
+                new Task(1,"Fix bug","In progress",10),
                 new Task(2,"Write tests","To Do", 0))));
-        programmers .add(new Programmer("John","Rio",List.of(
-                new Task(3,"Review code","Done",5),
+        programmers .add(new Programmer("John","Berlin",List.of(
+                new Task(3,"Refactor code","Done",7),
                 new Task(4,"Deploy application","In Progress",2)
+        )));
+        programmers.add(new Programmer("Charlie", "Munich", List.of(
+                new Task(5, "Code review", "In Progress", 6)
         )));
     }
 
@@ -26,20 +30,38 @@ class MainTest {
     void getAllTasks() {
         List<Task> allTasks = Main.getAllTasks(programmers);
         Assertions.assertFalse(allTasks.isEmpty());
-        assertEquals(4, allTasks.size());
+        assertEquals(5, allTasks.size());
 
         Task firstTask = allTasks.get(0);
         Assertions.assertAll(
                 () -> assertEquals(1, firstTask.getNumber()),
                 () -> assertEquals("Fix bug", firstTask.getDescription()),
                 () -> assertEquals("In progress", firstTask.getStatus()),
-                () -> assertEquals(3, firstTask.getDaysInProcessing()));
+                () -> assertEquals(10, firstTask.getDaysInProcessing()));
 
-        Task lastTask = allTasks.get(3);
+        Task lastTask = allTasks.get(4);
         Assertions.assertAll(
-                () -> assertEquals(4, lastTask.getNumber()),
-                () -> assertEquals("Deploy application", lastTask.getDescription()),
+                () -> assertEquals(5, lastTask.getNumber()),
+                () -> assertEquals("Code review", lastTask.getDescription()),
                 () -> assertEquals("In Progress", lastTask.getStatus()),
-                () -> assertEquals(2, lastTask.getDaysInProcessing()));
+                () -> assertEquals(6, lastTask.getDaysInProcessing()));
     }
-}
+    @Test
+    void filteredTasks(){
+        List<Task> filteredTasks = programmers.stream()
+                .filter(p -> "Berlin".equals(p.getCity()))
+                .flatMap(p -> p.getTasks().stream())
+                .filter(task -> !"Done".equalsIgnoreCase(task.getStatus()) && task.getDaysInProcessing() > 5)
+                .toList();
+
+        Assertions.assertFalse(filteredTasks.isEmpty());
+        Assertions.assertEquals(1, filteredTasks.size());
+
+        Task firstFilteredTask = filteredTasks.get(0);
+        Assertions.assertAll(
+                () -> assertEquals(1, firstFilteredTask.getNumber()),
+                () -> assertEquals("Fix bug", firstFilteredTask.getDescription()),
+                () -> assertEquals("In progress", firstFilteredTask.getStatus()),
+                () -> assertEquals(10, firstFilteredTask.getDaysInProcessing()));
+    }
+    }
